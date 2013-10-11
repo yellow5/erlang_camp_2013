69fa4f5c02d9183ee03bcc373976c34879318f24
Cameron Dykes <yellow5c@gmail.com>
2013-10-11 09:53:29 -0500

    Add ec_add:add/2 module and func

---
+++ essentials/ec_add.erl
@@ -0,0 +1,11 @@
+%%% @author Cameron Dykes
+%%% @doc contains add func
+%%% @copyright 2013 Erlware
+
+-module(ec_add).
+-export([add/2]).
+
+%% @doc adds two numbers together.
+-spec add(number(), number()) -> number().
+add(A, B) ->
+  A + B.
