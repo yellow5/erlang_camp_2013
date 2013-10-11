2f39231c88c2057b404d47fb9b3ca5d0acbfc5d4
Cameron Dykes <yellow5c@gmail.com>
2013-10-11 09:57:42 -0500

    Add ec_multiply:mult/3 func

--- essentials/ec_multiply.erl
+++ essentials/ec_multiply.erl
@@ -3,9 +3,14 @@
 %%% @copyright 2013 Erlware
 
 -module(ec_multiply).
--export([mult/2]).
+-export([mult/2, mult/3]).
 
 %% @doc multiplies two numbers together.
 -spec mult(number(), number()) -> number().
 mult(A, B) ->
   A * B.
+
+%% @doc multiplies three numbers together.
+-spec mult(number(), number(), number()) -> number().
+mult(A, B, C) ->
+  A * B * C.
