698fa76f3047d0c1f1600f2324dfc30cae7143da
Cameron Dykes <yellow5c@gmail.com>
2013-10-11 11:48:28 -0500

    Add ec_lists:twomult/1 func

--- essentials/ec_lists.erl
+++ essentials/ec_lists.erl
@@ -3,7 +3,7 @@
 %%% @copyright 2013 Erlware
 
 -module(ec_lists).
--export([print_each/1]).
+-export([print_each/1, twomult/1]).
 
 %% @doc recursivley print each element in the list
 -spec print_each(list()) -> string().
@@ -16,3 +16,14 @@ print_each([], _Count) ->
 print_each([H|Tail], Count) ->
   io:format("~p is ~p~n", [Count, H]),
   print_each(Tail, Count + 1).
+
+%% @doc recursively multiply all list values by 2
+-spec twomult(list()) -> list().
+twomult(List) ->
+  lists:reverse(twomult(List, [])).
+
+-spec twomult(list(), list()) -> list().
+twomult([], Acc) ->
+  Acc;
+twomult([H|Tail], Acc) ->
+  twomult(Tail, [H * 2|Acc]).
