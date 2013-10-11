f9228f35ed01902b879b6a12ec25ae7c978eb0fb
Cameron Dykes <yellow5c@gmail.com>
2013-10-11 10:27:17 -0500

    Add ec_math:not_less_than_2/1 func

--- essentials/ec_math.erl
+++ essentials/ec_math.erl
@@ -3,7 +3,7 @@
 %%% @copyright 2013 Erlware
 
 -module(ec_math).
--export([op/3]).
+-export([op/3, not_less_than_2/1]).
 
 %% @doc performs the operation on two values
 -spec op(add | sub, number(), number()) -> number().
@@ -11,3 +11,10 @@ op(add, A, B) ->
   A + B;
 op(sub, A, B) ->
   A - B.
+
+%% @doc identifies values that are not less than 2
+-spec not_less_than_2(number()) -> string().
+not_less_than_2(N) when N >= 2 ->
+  "N is greather than 2";
+not_less_than_2(_N) ->
+  "N is less than 2".
