189b55c911e61b633d47a5ed25844688ed342e3f
Cameron Dykes <yellow5c@gmail.com>
2013-10-11 10:55:22 -0500

    Add ec_recur:sum_seq/1 func

--- essentials/ec_recur.erl
+++ essentials/ec_recur.erl
@@ -3,7 +3,7 @@
 %%% @copyright 2013 Erlware
 
 -module(ec_recur).
--export([hr/1]).
+-export([hr/1, sum_seq/1]).
 
 %% @doc returns a string of dashes
 -spec hr(number()) -> string().
@@ -11,3 +11,13 @@ hr(1) ->
   "-";
 hr(N) ->
   "-" ++ hr(N - 1).
+
+%% @doc returns the sum of values less than a number
+-spec sum_seq(number()) -> number().
+sum_seq(N) ->
+  sum_seq(N, 0).
+
+sum_seq(0, Sum) ->
+  Sum;
+sum_seq(N, Sum) ->
+  sum_seq(N - 1, Sum + N).
